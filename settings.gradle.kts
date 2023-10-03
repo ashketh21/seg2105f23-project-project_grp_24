5b5d78b6a16af664d4520b463c65c79fa635f4de
nisarg <patelnisarg.3322@gmail.com>
2023-10-03 11:44:01 -0400

    initial commit created a project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Grimpeurs Cycling Club (GCC)"
+include(":app")
+ 
