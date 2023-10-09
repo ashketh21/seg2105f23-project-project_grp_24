24fd35dc9921f767a5e26ef7fab2f710090fb5b5
nisarg <patelnisarg.3322@gmail.com>
2023-10-09 09:56:55 -0400

    added firebase link

--- app/src/main/java/com/example/grimpeurscyclingclubgcc/MainActivity.java
+++ app/src/main/java/com/example/grimpeurscyclingclubgcc/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.grimpeurscyclingclubgcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onbuttonclick(View views){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://seg-project-gcc-default-rtdb.firebaseio.com\n");
+        DatabaseReference myName = database.getReference("user1/name");
+        DatabaseReference myage = database.getReference("user1/age");
+
+        DatabaseReference mygender = database.getReference("user1/gender");
+
+
+        myName.setValue("cody");
+        myage.setValue("32");
+        mygender.setValue("male");
     }
 }
